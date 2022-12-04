3fb012b6a3e5c021c903e97e8f07b0eb1427dbc7
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2022-12-03 22:44:45 -0500

    updated phone page

--- pages/finn/index.js
+++ pages/finn/index.js
@@ -2,8 +2,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function Finn() {
 	return (
-		<div className='flex w-screen h-screen min-h-screen flex-col items-center justify-center'>
-			<Spline scene='https://prod.spline.design/BBnma8cOa8oDBtYJ/scene.splinecode' />
+		<div className='flex w-full h-full items-center justify-center'>
+			<Spline  scene='https://prod.spline.design/BBnma8cOa8oDBtYJ/scene.splinecode' />
 		</div>
 	);
 }
--- pages/phone/index.js
+++ pages/phone/index.js
@@ -2,8 +2,16 @@ import Spline from '@splinetool/react-spline';
 
 export default function Phone() {
 	return (
-		<div className='flex w-screen h-screen min-h-screen flex-col items-center justify-center'>
-			<Spline scene='https://prod.spline.design/3ESsMmZPSkqA03kK/scene.splinecode' />
+		<div className='bg-black flex w-screen h-screen min-h-screen items-center justify-center'>
+			<div className=' bg-slate-800 h-full w-full py-4 px-7 flex flex-col items-start justify-center'>
+				<h2 className=' text-8xl text text-stone-200 font-bold'>Hello</h2>
+				<p className=' text-2xl text-stone-400 font-light'>
+					This is my phone ported into React using Spline
+				</p>
+			</div>
+			<div className=' bg-slate-500 h-full w-full p-4'>
+				<Spline scene='https://prod.spline.design/3ESsMmZPSkqA03kK/scene.splinecode' />
+			</div>
 		</div>
 	);
 }
