f88204c59c54e410c28b2a6017fa5ed133410389
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2022-12-03 22:08:07 -0500

    removed footer

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 					Welcome to my <a href='https://spline.design'>Spline</a> 3D Renders
 				</h1>
 
-				<div className={styles.grid} style={{marginTop: 20}}>
+				<div className={styles.grid} style={{ marginTop: 20 }}>
 					<Link href='/phone' className={styles.card}>
 						<h2>iPhone &rarr;</h2>
 						<p>View my iPhone home screen</p>
@@ -29,19 +29,6 @@ export default function Home() {
 					</Link>
 				</div>
 			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-					</span>
-				</a>
-			</footer>
 		</div>
 	);
 }
