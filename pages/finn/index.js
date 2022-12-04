01e652a1e5d9b478f305fcc260507bc4e77da48a
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2022-12-03 22:03:52 -0500

    added 2 scenes and Tailwind

---
+++ pages/finn/index.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Finn() {
+	return (
+		<div className='flex w-screen h-screen min-h-screen flex-col items-center justify-center'>
+			<Spline scene='https://prod.spline.design/BBnma8cOa8oDBtYJ/scene.splinecode' />
+		</div>
+	);
+}
---
+++ pages/phone/index.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Phone() {
+	return (
+		<div className='flex w-screen h-screen min-h-screen flex-col items-center justify-center'>
+			<Spline scene='https://prod.spline.design/3ESsMmZPSkqA03kK/scene.splinecode' />
+		</div>
+	);
+}
